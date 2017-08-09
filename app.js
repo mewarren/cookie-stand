cb68051afb0aa225ab5d678fd912f7b2371204c4
mewarren <holla@mewarren.com>
2017-08-08 18:12:54 -0700

    created constructor function, converted list to table

--- app.js
+++ app.js
@@ -1,222 +1,89 @@
 'use strict';
 
-var airport = {
-    location: "PDX Airport",
-    minCust: 23,
-    maxCust: 65,
-    aveCookSale: 6.3,
-    cookPerHour: [],
-    totalCookies: 0,
-    storeHours: ['6am : ','7am : ','8am : ','9am : ','10am : ','11am : ','12pm : ','1pm : ','2pm : ','3pm : ','4pm : ','5pm : ','6pm : ','7pm : ','8pm : ', 'Total : '],
-    
-    randomNumber: function() {
-      for(var i =0; i < 14; i++){
+var storeHours= ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm', 'Total'];
+
+function Store( location, minCust, maxCust, aveCookSale, id ){
+    this.location = location;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.id = id;    
+    this.aveCookSale=aveCookSale;
+    this.cookPerHour=[];
+    this.totalCookies=0;
+}
+
+Store.prototype.randomNumber = function() {
+    for(var i =0; i < 15; i++){
         var numberPeople = Math.floor (Math.random() * (this.maxCust - this.minCust) + this.minCust);
         var numberCookies = Math.floor(this.aveCookSale * numberPeople);
         this.cookPerHour.push(numberCookies);
       }  
-    },
+};
 
-    getTotal: function(){
-        for(var i =0; i < 14; i++){
+Store.prototype.getTotal = function(){
+        for(var i =0; i < 15; i++){
             this.totalCookies += this.cookPerHour[i];
         }
-    },
+}; 
 
-    run: function(){
-        this.randomNumber();
-        console.log( this.cookPerHour);
-        this.getTotal(); 
-        console.log(this.totalCookies);   
-    }, 
+Store.prototype.addToThead = function(){
+    var table = document.getElementsByTagName('thead')[0];
+    var tablerow = document.createElement('tr');
     
-    addToDom: function(){
-        //this.getTotal();
-        this.randomNumber();
-        var element = document.getElementById('airport');
-        for( var i=0; i < 14; i++){
-            var list = document.createElement('li');
-            list.innerHTML = this.storeHours[i] + this.cookPerHour[i];
-            element.appendChild( list); 
-        }
+    var hoursCell = document.createElement('th');
+    hoursCell.innerHTML = 'Time';
+    tablerow.appendChild(hoursCell);   
+    
+    for( var i=0; i <= 15; i++){
+        var hoursCell = document.createElement('th');
+        hoursCell.innerHTML = storeHours[i];
+        tablerow.appendChild(hoursCell); 
     }
-}
-airport.addToDom();
+    table.appendChild(tablerow);
 
+};
 
-var pioneer = {
-    location: "Pioneer Square",
-    minCust: 3,
-    maxCust: 24,
-    aveCookSale: 1.2,
-    cookPerHour: [],
-    totalCookies: 0,
-    storeHours: ['6am : ','7am : ','8am : ','9am : ','10am : ','11am : ','12pm : ','1pm : ','2pm : ','3pm : ','4pm : ','5pm : ','6pm : ','7pm : ','8pm : ', 'Total : '],
+Store.prototype.addToTbody = function(){
+ 
+    var table = document.getElementsByTagName('tbody')[0];
+    var tablerow = document.createElement('tr');
+    this.randomNumber();
     
-    randomNumber: function() {
-      for(var i =0; i < 14; i++){
-        var numberPeople = Math.floor (Math.random() * (this.maxCust - this.minCust) + this.minCust);
-        var numberCookies = Math.floor(this.aveCookSale * numberPeople);
-        this.cookPerHour.push(numberCookies);
-      }  
-    },
+    var cookiesCell = document.createElement('td');
+    cookiesCell.innerHTML = this.location;
+    tablerow.appendChild(cookiesCell);
 
-    getTotal: function(){
-        for(var i =0; i < 14; i++){
-            this.totalCookies += this.cookPerHour[i];
-        }
-    },
-
-    run: function(){
-        this.randomNumber();
-        console.log( this.cookPerHour);
-        this.getTotal(); 
-        console.log(this.totalCookies);   
-    }, 
-    
-    addToDom: function(){
-        //this.getTotal();
-        this.randomNumber();
-        var element = document.getElementById('pioneer');
-        for( var i=0; i < 14; i++){
-            var list = document.createElement('li');
-            list.innerHTML = this.storeHours[i] + this.cookPerHour[i];
-            element.appendChild( list); 
-        }
+    for( var i=0; i <= 14; i++){
+        var cookiesCell = document.createElement('td');
+        cookiesCell.innerHTML = this.cookPerHour[i];
+        tablerow.appendChild(cookiesCell); 
     }
-}
-pioneer.addToDom();
+    this.getTotal();
+    var cookiesCell = document.createElement('td');
+    cookiesCell.innerHTML = this.totalCookies;
+    tablerow.appendChild(cookiesCell);
 
-var powells = {
-    location: "Powell's",
-    minCust: 11,
-    maxCust: 38,
-    aveCookSale: 3.7,
-    cookPerHour: [],
-    totalCookies: 0,
-    storeHours: ['6am : ','7am : ','8am : ','9am : ','10am : ','11am : ','12pm : ','1pm : ','2pm : ','3pm : ','4pm : ','5pm : ','6pm : ','7pm : ','8pm : ', 'Total : '],
-    
-    randomNumber: function() {
-      for(var i =0; i < 14; i++){
-        var numberPeople = Math.floor (Math.random() * (this.maxCust - this.minCust) + this.minCust);
-        var numberCookies = Math.floor(this.aveCookSale * numberPeople);
-        this.cookPerHour.push(numberCookies);
-      }  
-    },
-
-    getTotal: function(){
-        for(var i =0; i < 14; i++){
-            this.totalCookies += this.cookPerHour[i];
-        }
-    },
-
-    run: function(){
-        this.randomNumber();
-        console.log( this.cookPerHour);
-        this.getTotal(); 
-        console.log(this.totalCookies);   
-    }, 
-    
-    addToDom: function(){
-        //this.getTotal();
-        this.randomNumber();
-        var element = document.getElementById('powells');
-        for( var i=0; i < 14; i++){
-            var list = document.createElement('li');
-            list.innerHTML = this.storeHours[i] + this.cookPerHour[i];
-            element.appendChild( list); 
-        }
-    }
-}
-powells.addToDom();
+    table.appendChild(tablerow);
+};
+       
+var airport = new Store ( 'PDX Airport', 23, 65, 6.3, 'airport' );
+var powells = new Store ( 'Powell\'s', 11, 38, 3.7, 'powells' );
+var pioneer = new Store ( 'Pioneer Square', 3, 24, 1.2, 'pioneer' );
+var stjohns = new Store ('St. John\'s', 20, 38, 2.3, 'stjohns');
+var waterfront = new Store ( 'Waterfront', 2, 12, 4.6, 'waterfront' );
 
 
-var stjohns = {
-    location: "St. John's",
-    minCust: 20,
-    maxCust: 38,
-    aveCookSale: 2.3,
-    cookPerHour: [],
-    totalCookies: 0,
-    storeHours: ['6am : ','7am : ','8am : ','9am : ','10am : ','11am : ','12pm : ','1pm : ','2pm : ','3pm : ','4pm : ','5pm : ','6pm : ','7pm : ','8pm : ', 'Total : '],
-    
-    randomNumber: function() {
-      for(var i =0; i < 14; i++){
-        var numberPeople = Math.floor (Math.random() * (this.maxCust - this.minCust) + this.minCust);
-        var numberCookies = Math.floor(this.aveCookSale * numberPeople);
-        this.cookPerHour.push(numberCookies);
-      }  
-    },
-
-    getTotal: function(){
-        for(var i =0; i < 14; i++){
-            this.totalCookies += this.cookPerHour[i];
-        }
-    },
-
-    run: function(){
-        this.randomNumber();
-        console.log( this.cookPerHour);
-        this.getTotal(); 
-        console.log(this.totalCookies);   
-    }, 
-    
-    addToDom: function(){
-        //this.getTotal();
-        this.randomNumber();
-        var element = document.getElementById('stjohns');
-        for( var i=0; i < 14; i++){
-            var list = document.createElement('li');
-            list.innerHTML = this.storeHours[i] + this.cookPerHour[i];
-            element.appendChild( list); 
-        }
-    }
-}
-stjohns.addToDom();
+airport.addToThead();
+airport.addToTbody();
+powells.addToTbody();
+pioneer.addToTbody();
+stjohns.addToTbody();
+waterfront.addToTbody();
 
 
-var waterfront = {
-    location: "Waterfront",
-    minCust: 2,
-    maxCust: 16,
-    aveCookSale: 4.6,
-    cookPerHour: [],
-    totalCookies: 0,
-    storeHours: ['6am : ','7am : ','8am : ','9am : ','10am : ','11am : ','12pm : ','1pm : ','2pm : ','3pm : ','4pm : ','5pm : ','6pm : ','7pm : ','8pm : ', 'Total : '],
-    
-    randomNumber: function() {
-      for(var i =0; i < 14; i++){
-        var numberPeople = Math.floor (Math.random() * (this.maxCust - this.minCust) + this.minCust);
-        var numberCookies = Math.floor(this.aveCookSale * numberPeople);
-        this.cookPerHour.push(numberCookies);
-      }  
-    },
 
-    getTotal: function(){
-        for(var i =0; i < 14; i++){
-            this.totalCookies += this.cookPerHour[i];
-        }
-    },
 
-    run: function(){
-        this.randomNumber();
-        console.log( this.cookPerHour);
-        this.getTotal(); 
-        console.log(this.totalCookies);   
-    }, 
-    
-    addToDom: function(){
-        //this.getTotal();
-        this.randomNumber();
-        var element = document.getElementById('waterfront');
-        for( var i=0; i < 14; i++){
-            var list = document.createElement('li');
-            list.innerHTML = this.storeHours[i] + this.cookPerHour[i];
-            element.appendChild( list); 
-        }
-    }
-}
-waterfront.addToDom();
+
 
 
 
